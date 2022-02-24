6636a1c78928b40c6a768f0bf369232a93b906f1
Charles Vernerey <charlesvernerey2@gmail.com>
2022-02-24 19:09:00 +0100

    Update project

--- choco/Project.java
+++ choco/Project.java
@@ -3,12 +3,12 @@ import java.util.List;
 public class Project {
 
     public String name;
-    public String duration;
+    public int duration;
     public int score;
     public int bestBefore;
     public List<Skill> roles;
 
-    public Project(String name, String duration, int score, int bestBefore, List<Skill> roles) {
+    public Project(String name, int duration, int score, int bestBefore, List<Skill> roles) {
         this.name = name;
         this.duration = duration;
         this.score = score;
