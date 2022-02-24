a9276061c9a638f6933d7d049e8b0f1c468a144a
Charles Vernerey <charlesvernerey2@gmail.com>
2022-02-24 20:02:52 +0100

    Add hasMentor in skill

--- choco/Skill.java
+++ choco/Skill.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class Skill {
 
     public String name;
@@ -6,5 +8,9 @@ public class Skill {
     public Skill(String name, int level) {
         this.name = name;
         this.level = level;
+    }
+
+    public boolean hasMentor(List<Person> persons) {
+        return persons.stream().anyMatch(p -> p.canDo(this));
     }
 }
